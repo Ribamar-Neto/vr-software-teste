662666be815e1dbb3685c250a58ad92f62801d5c
Ribamar Neto <ribaferreiraneto@gmail.com>
2025-08-07 16:54:42 -0300

    Feat: Add from entity staticmethod to schemas

--- src/schemas/create_notification_output.py
+++ src/schemas/create_notification_output.py
@@ -3,8 +3,16 @@ from __future__ import annotations
 from uuid import UUID
 
 from pydantic import BaseModel, Field
+from src.entities import Notification
 
 
 class CreateNotificationOutput(BaseModel):
     message_id: UUID | None = Field(default=None, examples=['123e4567-e89b-12d3-a456-426614174000'])
     trace_id: UUID | None = Field(default=None, examples=['224e4687-e89b-12d3-a456-426697174000'])
+
+    @staticmethod
+    def from_entity(self, notification: Notification) -> CreateNotificationOutput:
+        return CreateNotificationOutput(
+            trace_id=notification.trace_id,
+            message_id=notification.message_id,
+        )
