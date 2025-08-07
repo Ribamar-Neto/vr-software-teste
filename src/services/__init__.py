214782e21d27f2cb49e37a001d28dfeb9b9fa3db
Ribamar Neto <ribaferreiraneto@gmail.com>
2025-08-07 17:34:09 -0300

    Feat: services and settings

---
+++ src/services/__init__.py
@@ -0,0 +1,2 @@
+from .notification_service import NotificationService
+from .notification_service_rabbitmq import RabbitMQNotificationService
---
+++ src/settings/broker/broker_settings.py
@@ -0,0 +1,38 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from src.enums import EnvironmentEnum
+
+
+class BrokerSettings(BaseSettings):
+    BROKER_USERNAME: str | None = None
+    BROKER_PASSWORD: str | None = None
+    BROKER_HOST: str | None = None
+    BROKER_PORT: int | None = None
+    ENVIRONMENT: str = EnvironmentEnum.DEVELOPMENT
+
+    @property
+    def url(self) -> str:
+        if self.ENVIRONMENT == EnvironmentEnum.TESTING:
+            return 'amqp://guest:guest@localhost:5673/'
+        return f'amqp://{self.BROKER_USERNAME}:{self.BROKER_PASSWORD}@{self.BROKER_HOST}:{self.BROKER_PORT}/'
+
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='allow')
+
+    @property
+    def queue_input_name(self) -> str:
+        return 'fila.notificacao.entrada.ivarcarrera'
+
+    @property
+    def queue_retry_name(self) -> str:
+        return 'fila.notificacao.retry.ivarcarrera'
+
+    @property
+    def queue_validation_name(self) -> str:
+        return 'fila.notificacao.validacao.ivarcarrera'
+
+    @property
+    def queue_dlq_name(self) -> str:
+        return 'fila.notificacao.dlq.ivarcarrera'
+
+
+broker_settings = BrokerSettings()
